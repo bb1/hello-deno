e361fd7d4f07e847188e58695243b9c059c228f3
Birk Brauer <3842623+bb1@users.noreply.github.com>
2021-03-01 23:04:31 +0100

    added login

---
+++ src/http/post-login/index.ts
@@ -0,0 +1,33 @@
+
+export async function handler (req: any) {
+  // TODO: auth
+  if (req.params.username) {
+
+  }
+
+  const token = '';
+
+  return {
+    statusCode: 200,
+    headers: {
+      'content-type': 'application/json; charset=utf8',
+      'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
+    },
+    body: JSON.stringify({
+      token,
+    })
+  }
+}
+
+
+/* Deliver client-side JS
+export async function handler (req: object) {
+  return {
+    headers: {
+      'content-type': 'text/javascript; charset=utf8',
+      'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
+    },
+    body: 'console.log("Hello world!")',
+  }
+}
+*/
