5a1d9b943e86e22a3228bfbb38d1d7edd46658aa
Birk Brauer <3842623+bb1@users.noreply.github.com>
2021-03-01 21:19:07 +0100

    prepared /data

---
+++ src/http/get-data/index.ts
@@ -0,0 +1,28 @@
+
+export async function handler (req: object) {
+  // TODO: auth
+
+  return {
+    statusCode: 200,
+    headers: {
+      'content-type': 'application/json; charset=utf8',
+      'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
+    },
+    body: JSON.stringify({
+      data: 'Hello world'
+    })
+  }
+}
+
+
+/* Deliver client-side JS
+export async function handler (req: object) {
+  return {
+    headers: {
+      'content-type': 'text/javascript; charset=utf8',
+      'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
+    },
+    body: 'console.log("Hello world!")',
+  }
+}
+*/
