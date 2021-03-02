3415e86c8e3ed35e1166dd7b96ff7974a06b7e10
Birk Brauer <3842623+bb1@users.noreply.github.com>
2021-03-02 22:44:18 +0100

    disabled some rules

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './index.css';
 
+/* eslint-disable no-use-before-define */
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
+/* eslint-enable no-use-before-define */
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
